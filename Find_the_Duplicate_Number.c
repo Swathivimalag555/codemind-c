bee561cfe74dc4f2bff8c2c103eb5163a53f763b
Swathivimalag555 <97424553+Swathivimalag555@users.noreply.github.com>
2022-10-06 15:49:46 +0530

    #include<stdio.h> int main() {     int n,a[100],i,j,c;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(i!=j)             {             if(a[i]==a[j])             {                 printf("%d",a[i]);                 c=1;                 break;             }         }     }     if(c==1)     {         break;     } } }

---
+++ Find_the_Duplicate_Number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,j,c;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+            if(a[i]==a[j])
+            {
+                printf("%d",a[i]);
+                c=1;
+                break;
+            }
+        }
+    }
+    if(c==1)
+    {
+        break;
+    }
+}
+}
