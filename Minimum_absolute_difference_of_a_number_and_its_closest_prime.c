b0268b94a89d685d99fe99c27fe3f9c53734b030
Swathivimalag555 <97424553+Swathivimalag555@users.noreply.github.com>
2022-10-14 19:50:31 +0530

    #include<stdio.h> int prime(int num) {     int i;     for (i=2; i*i<=num; i++)     {         if (num%i==0)         {             return 0;         }     }     return 1; } int main() {     int a;     scanf("%d",&a);     int diff1=0,diff2=0,i,p1,p2;     for (i=a+1; i<=10000; i++)     {         if (prime(i)==1)         {             diff1=i-a;             p1=i;             break;         }     }     for (i=a; i>0; i--)     {         if (prime(i)==1)         {             diff2=a-i;             p2=i;             break;         }     }     if (diff1>diff2)     {         printf("%d",diff2);     }     else if (diff1<diff2)     {         printf("%d",diff1);     }     else     {         printf("%d",diff2);     }     return 0; }

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
@@ -1,46 +1,50 @@
 #include<stdio.h>
-#include<math.h>
-int isprime(int num)
+int prime(int num)
 {
-    int i,fc=0;
-	for(i=1;i<=num;i++)
-	{
-		if(num%i==0)
-		{
-			fc++;	
-		}	
-	}
-	if(fc==2)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+    int i;
+    for (i=2; i*i<=num; i++)
+    {
+        if (num%i==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
 }
 int main()
 {
-	int num,pp,np,avg,res,r1,r2;
-	float sq;
-	scanf("%d",&num);
-	if(isprime(num))
-	{
-	    printf("0");
-	}
-	else
-	{
-    	for(pp=num-1;!isprime(pp);pp--);
-    	for(np=num+1;!isprime(np);np++);
-    	r1=np-num;
-    	r2=num-pp;
-    	if(r1>r2)
-    	{
-    	    printf("%d",r2);
-    	}
-    	else
-    	{
-    	    printf("%d",r1);
-    	}
-	}
+    int a;
+    scanf("%d",&a);
+    int diff1=0,diff2=0,i,p1,p2;
+    for (i=a+1; i<=10000; i++)
+    {
+        if (prime(i)==1)
+        {
+            diff1=i-a;
+            p1=i;
+            break;
+        }
+    }
+    for (i=a; i>0; i--)
+    {
+        if (prime(i)==1)
+        {
+            diff2=a-i;
+            p2=i;
+            break;
+        }
+    }
+    if (diff1>diff2)
+    {
+        printf("%d",diff2);
+    }
+    else if (diff1<diff2)
+    {
+        printf("%d",diff1);
+    }
+    else
+    {
+        printf("%d",diff2);
+    }
+    return 0;
 }
